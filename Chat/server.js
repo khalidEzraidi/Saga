af6964d791477f084d0b671cd3432714990f6d2c
Jean Hameau <jr.hameau@gmail.com>
2019-10-24 11:59:01 +0200

    reorganizing files

--- Chat/server.js
+++
@@ -1,9 +0,0 @@
-const io = require('socket.io')(2019)
-
-io.on('connection', socket => {
-    socket.emit('chat-message', 'Welcome to Saga')
-
-    socket.on('send-chat-message', message => {
-        socket.emit('chat-message', 'You said : ' + message)
-    })
-})
