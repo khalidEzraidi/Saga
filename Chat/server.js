be1e16fba8f255a60ad00bd32513842a42f68e40
Jean Hameau <jr.hameau@gmail.com>
2019-10-24 11:33:57 +0200

    Implementing a stupid bot repeating all you send him

--- Chat/server.js
+++ Chat/server.js
@@ -1,10 +1,9 @@
 const io = require('socket.io')(2019)
 
 io.on('connection', socket => {
-    console.log('New Connection')
     socket.emit('chat-message', 'Welcome to Saga')
+
     socket.on('send-chat-message', message => {
-        console.log(message)
-        socket.broadcast.emit('chat-message', message)
+        socket.emit('chat-message', 'You said : ' + message)
     })
 })
